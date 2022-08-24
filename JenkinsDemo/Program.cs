4f1b050e2da7c9e0286cc4f47e6a4f93ddf28680
PavanPolepaka123 <111893375+PavanPolepaka123@users.noreply.github.com>
2022-08-25 00:33:45 +0530

    Add project files.

---
+++ JenkinsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JenkinsDemo;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            int ac = p.Add(1, 2);
+            Assert.AreEqual(3, ac);
+        }
+    }
+}
